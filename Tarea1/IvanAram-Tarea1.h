9258bc0ef4d8edb6cff1fba2bd312de504580044
IvanAram <a01022584@itesm.mx>
2016-08-12 05:03:44 -0500

    Tarea1

---
+++ Tarea1/IvanAram-Tarea1.h
@@ -0,0 +1,36 @@
+/*
+	Ivan Aram Gonzalez Su
+	A01022584
+*/
+
+#pragma once
+#include <iostream>
+
+class Computer{
+private:
+	void seleccionComponentes();
+	void ensambleComponentes();
+	void configSoftware();
+	void empaquetamiento();
+public:
+	void createLaptop();
+	void createDesktop();
+	void createNotebook();
+	void createTablet();
+};
+
+void Computer::createLaptop(){
+
+}
+
+void Computer::createDesktop(){
+
+}
+
+void Computer::createNotebook(){
+
+}
+
+void Computer::createTablet(){
+
+}
